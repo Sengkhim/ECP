8675e05e4e383070d1c1e4959d08a7b3db14dcb5
Sengkhim <75316691+Sengkhim@users.noreply.github.com>
2025-02-19 15:13:41 +0700

    conifg api-gateway

--- API-GateWay/Program.cs
+++ API-GateWay/Program.cs
@@ -1,21 +1,49 @@
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-builder.Services.AddControllers();
+builder.Host.UseSerilog();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+
+builder.Services.AddHealthChecks()
+    .AddCheck("API Gateway Health", () => HealthCheckResult.Healthy());
+
+builder.Services.AddResponseCaching();
+
+builder.Services
+    .AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseResponseCaching();
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
-app.MapControllers();
+app.UseRateLimiter();
+app.MapReverseProxy();
+app.MapHealthChecks("/health");
 app.Run();
--- API-GateWay/obj/Debug/net8.0/API-GateWay.AssemblyInfo.cs
+++ API-GateWay/obj/Debug/net8.0/API-GateWay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API-GateWay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f617c8e3bea0418c3493978d00c1aa88d68560da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8975e2c599205a692f4be861649b679cdfb98dfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("API-GateWay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API-GateWay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
